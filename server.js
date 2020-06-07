ff8045d35bbb2d5e6776d6e39e4cd752d43b5900
Bryson Creighton <brysoncreighton@gmail.com>
2020-06-06 22:31:15 -0600

    Pre production deploy
    Remove security vulnerabilities
    
    remove logs
    
    establish server start reqs
    
    handle server errors

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const MOVIEDEX = require('./MOVIEDEX.json')
 
 const app = express();
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
@@ -70,8 +71,18 @@ handleGetMovies = (req, res) => {
 // ROUTES
 app.get('/movies', handleGetMovies)
 
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } }
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
 // SERVER LISTENING
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
   console.log(`Server is listening on PORT ${PORT}`)
